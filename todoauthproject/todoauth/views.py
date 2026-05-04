affb7c1899c2ee010c5820b277af33c62076b888
Rijo007 <rijovarghese225@gmail.com>
2026-05-04 22:43:09 +0530

    newcommit

--- todoauthproject/todoauth/views.py
+++ todoauthproject/todoauth/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
 from todoauth import models
+from django.contrib.auth import authenticate,login
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def signup(request):
@@ -12,7 +14,7 @@ def signup(request):
         password=request.POST.get("password")
         # conform_password=request.POST.get("conform_password")      
         if User.objects.filter(username=username).exists():
-            messages.error(request,"password does not matches")
+            messages.error(request,"username already exists")
             
             return redirect("signup")
 
@@ -24,16 +26,35 @@ def signup(request):
         
         messages.success(request,"Account Created successfully , please login")
         return redirect('login')
-    return render(request,'login.html')
+    return render(request,'signup.html')
 
 
 
 
-def login(request):
-    
+def login_view(request):
+    if request.method=="POST":
+        username=request.POST.get("username")
+        email=request.POST.get("email")
+        password=request.POST.get("password")
+        
+        user=authenticate(request,username=username,password=password)
+        
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+            messages.success(request,"password doesnt match plese try again")
+            
+        else:
+            return redirect('login')
+        
+            messages.error(request,"password doesnt match plese try again")
     
     
     
     return render(request,'login.html')
    
                 
+@login_required
+def homepage(request):
+        
+        return render(request,'todopage.html')            
--- todoauthproject/todoauthproject/urls.py
+++ todoauthproject/todoauthproject/urls.py
@@ -21,5 +21,6 @@ from todoauth import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.signup,name='signup'),
-    path('',views.login,name='login')
+    path('login',views.login_view,name='login'),
+    path('home',views.homepage,name="home")
 ]
